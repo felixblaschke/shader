56d0e08b7cadcd89c8e7e31c0b1eaaf046120c55
Felix Blaschke <felix@felix-blaschke.de>
2022-06-18 10:59:09 +0200

    Rename lib file to match package validation

--- bin/shader.dart
+++ bin/shader.dart
@@ -1,4 +1,4 @@
-import 'package:shader/shader_run.dart';
+import 'package:shader/shader.dart';
 
 /// Entry point for executable `shader`
 Future<void> main(List<String> args) async => runCliApplication(args);
